ca6e3f8c00fa3a2a861ea27d6e72856084c8935d
Yunuen AM <yunameza@outlook.com>
2023-04-27 15:53:22 -0600

    Feat : main routes js added

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,9 +1,25 @@
-const routerCharacter = require('./characters/index');
-
+const express = require('express');
+const routerCharacter = require('./characters');
 
 const routerApi = (app) => {
-    app.use('/character', routerCharacter);
-    // app.use('/trainers', routerTrainers);
-}
+  app.use(express.json());
+  app.use('/characters', routerCharacter);
+  app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+  });
+};
 
 module.exports = routerApi;
+
+
+
+// const express = require('express');
+// const routerCharacter = require('./characters/index');
+
+
+// const routerApi = (app) => {
+//     app.use('/character', routerCharacter);
+//     // app.use('/trainers', routerTrainers);
+// }
+
+// module.exports = routerApi;
